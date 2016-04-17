a7a281e32e2fb757aa1784830da3293d02f6d13b
yichen <yichen@yichendeMac.local>
2016-04-17 23:28:10 +0800

    我的第一个swife代码

---
+++ myFirstSwiftDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myFirstSwiftDemo
+//
+//  Created by yichen on 16-4-16.
+//  Copyright (c) 2016年 yichen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
