66c5020274f1b9200b1dd0fd3ab89f64b4a8ba22
yichen <yichen@yichendeMac.local>
2016-04-19 23:25:27 +0800

    第三次提交测试，这是用xcode直接提交的

--- myFirstSwiftDemo/ViewController.swift
+++ myFirstSwiftDemo/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        NSLog("%@", "my swift hello world")
+        NSLog("%@", "I will be a big man")
     }
 
     override func didReceiveMemoryWarning() {
